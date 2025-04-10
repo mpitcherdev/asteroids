43d68a0c458b227375e5ceab0f047bc8f6f9ce45
mpitcherdev <mpitcher.dev@gmail.com>
2025-04-09 20:39:35 -0400

    2nd commit, player able to shoot, no collision

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        # Draw a circle on the given surface
+        # Parameters: surface, color, position, radius, width
+        pygame.draw.circle(screen, (255, 255, 255), (int(self.position.x), int(self.position.y)), self.radius, 2)
+
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -2,17 +2,52 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
+import pygame, sys
 from constants import *
+from player import *
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
+	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+	dt = 0
+
+	# groups
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	# containers
+	Asteroid.containers = (asteroids, updatable, drawable)
+	Player.containers = (updatable, drawable)
+	AsteroidField.containers = (updatable,)
+	Shot.containers = (shots, updatable, drawable)
+	asteroid_field = AsteroidField()
+
+	player_obj = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 	while True:
 		for event in pygame.event.get():
-   			if event.type == pygame.QUIT:
-       				return
-		screen.fill("black") 
+			if event.type == pygame.QUIT:
+				return
+		
+		updatable.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.collision(player_obj):  # If collision is detected
+				print("Game over!")
+				sys.exit()  # Exit the program\
+
+		screen.fill("black")
+		for obj in drawable:
+			obj.draw(screen)
 		pygame.display.flip()
+		
+		# 60 fps limit
+		dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
